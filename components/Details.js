d68393bd4cefe6a02ceb7dac772e224fafd94e2c
Andrés Salazar <andreseduardo1705@outlook.es>
2019-06-14 06:22:22 -0500

    Las consultas a API me funcionan perfecto

--- components/Details.js
+++ components/Details.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class DetailsScreen extends React.Component {
+
+    static navigationOptions = {
+      header: null
+    }  
     render() {
       return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { connect } from 'react-redux'
+import axios from 'axios'
 
 function mapStateToProps(state){
   return {
@@ -9,17 +10,38 @@ function mapStateToProps(state){
 }
 
 class HomeScreen extends React.Component 
-{
+{    
+  static navigationOptions = {
+      header: null
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }  
+  async componentDidMount(){
+    let todos = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+
+    this.setState({todos: todos});
+    console.log(this.state);
+  }
   render() {
+
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>{this.props.state.mensaje}</Text>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>    
         <Button 
           title="btn_details" 
           onPress={() => this.props.navigation.navigate('Details')} 
           color="green"
         >
-          Details
+          Ir a detalles
         </Button>
       </View>
     );
